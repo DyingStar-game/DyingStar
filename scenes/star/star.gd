51f4763364a0e248aa83f067505e68954e812c5a
koothka <koothka@gmail.com>
2025-09-04 15:49:12 +0200

    Test

--- scenes/star/star.gd
+++ scenes/star/star.gd
@@ -1,7 +1,7 @@
 extends MeshInstance3D
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if multiplayer.is_server(): return
 	var camera = get_viewport().get_camera_3d()
 	if camera:
