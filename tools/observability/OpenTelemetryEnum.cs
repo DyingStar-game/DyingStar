d607090a805d9e2aba1e91e263df275a5e52be9d
TigerWhiteDev <damiac83@gmail.com>
2025-09-13 14:06:57 +0200

    add obs class for call observability on gdscript and add config server and client + simplify add metrics and trace

---
+++ tools/observability/OpenTelemetryEnum.cs
@@ -0,0 +1,20 @@
+
+public enum MetricType
+{
+    Counter,
+    UpDownCounter,
+    Histogram,
+    ObservableGauge,
+    ObservableCounter,
+    ObservableUpDownCounter
+}
+
+public enum LogLevelType
+{
+    Trace,
+    Debug,
+    Information,
+    Warning,
+    Error,
+    Critical
+}
