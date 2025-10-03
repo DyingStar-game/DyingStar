7f913d8e13164ef6d9c22b06b14a1f88e7a45baa
TigerWhiteDev <damiac83@gmail.com>
2025-10-03 22:26:49 +0200

    add custom logger for send base print on logs centralized

---
+++ tools/observability/custom_logger.gd
@@ -0,0 +1,15 @@
+extends Logger
+
+class_name  CustomLogger
+
+func _log_error(function: String, file: String, line: int, code: String, rationale: String, editor_notify: bool, error_type: int, script_backtraces: Array[ScriptBacktrace]) -> void:
+	pass;
+
+
+func _log_message(message: String, error: bool) -> void:
+	if message.contains("OBS"):	
+		return
+	if error:	
+		Obs.logs_error("",message);
+	else :
+		Obs.logs_info("", message)
