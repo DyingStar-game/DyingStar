554313b907e7acc63808810df5ee4784e91c4570
David Durieux <david@durieux.family>
2025-10-04 15:43:26 +0200

    Continue code with others :D

--- scenes/props/testbox/box_50_cm.gd
+++ scenes/props/testbox/box_50_cm.gd
@@ -11,17 +11,18 @@ var type_name = "box50cm"
 var spawn_position: Vector3 = Vector3.ZERO
 var spawn_rotation: Vector3 = Vector3.UP
 
-var server_last_global_position = Vector3.ZERO
+var server_last_position = Vector3.ZERO
 var server_last_global_rotation = Vector3.ZERO
 
 @onready var is_inside_box4m: bool = false
 
 func _ready() -> void:
-	global_position = spawn_position
+	freeze = true
+	position = spawn_position
 
 func _physics_process(_delta: float) -> void:
 	if GameOrchestrator.is_server():
-		if server_last_global_position != global_position or server_last_global_rotation != global_rotation:
-			emit_signal("hs_server_prop_move", uuid, global_position, global_rotation, "box50cm")
-			server_last_global_position = global_position
+		if server_last_position != position or server_last_global_rotation != global_rotation:
+			emit_signal("hs_server_prop_move", uuid, position, global_rotation, "box50cm", null, true)
+			server_last_position = position
 			server_last_global_rotation = global_rotation
