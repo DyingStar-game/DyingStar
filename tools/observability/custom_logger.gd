850faa5f88b93d55d58145f3f65bc904c4e5c929
TigerWhiteDev <damiac83@gmail.com>
2025-10-04 15:09:27 +0200

    fix grean linter

--- tools/observability/custom_logger.gd
+++ tools/observability/custom_logger.gd
@@ -1,15 +1,21 @@
+class_name CustomLogger
 extends Logger
-
-class_name  CustomLogger
-
-#func _log_error(function: String, file: String, line: int, code: String, rationale: String, editor_notify: bool, error_type: int, script_backtraces: Array[ScriptBacktrace]) -> void:
-#	pass;
-
+# func _log_error(
+# 	function: String,
+# 	file: String,
+# 	line: int,
+# 	code: String,
+# 	rationale: String,
+# 	editor_notify: bool,
+# 	error_type: int,
+# 	script_backtraces: Array[ScriptBacktrace]
+# 	) -> void:
+# 	pass;
 
 func _log_message(message: String, error: bool) -> void:
-	if message.contains("OBS"):	
+	if message.contains("OBS"):
 		return
-	if error:	
+	if error:
 		Obs.logs_error("",message);
 	else :
 		Obs.logs_info("", message)
--- tools/observability/obs.gd
+++ tools/observability/obs.gd
@@ -24,15 +24,18 @@ func _ready() -> void:
 # LOG
 
 func print_colored(level: String, section: String, message: String) -> void:
-	var sectionColor = sections.get(section,"white")
-	var levelColor = levels.get(level,"white")
-	var ownerColor = "white"
+	var section_color = sections.get(section,"white")
+	var level_color = levels.get(level,"white")
+	var owner_color = "white"
 	if OS.has_feature("dedicated_server"):
-		ownerColor = "white"
+		owner_color = "white"
 	else :
-		ownerColor = "brown"
-	
-	print_rich("OBS.[color="+sectionColor+"][b]["+section+"][/b][/color] [color="+levelColor+"]"+level+"[/color] [color="+ownerColor+"] "+message+" [/color]")
+		owner_color = "brown"
+	print_rich(
+		"OBS.[color="+section_color+"][b]["+section+\
+		"][/b][/color] [color="+level_color+"]"+\
+		level+"[/color] [color="+owner_color+"] "+message+" [/color]"
+	)
 
 func logs_trace(section: String, message: String, tags: Dictionary = {}) -> void:
 	if 0 >= otel_manager.GDS_GetLogLevelType() :
@@ -48,9 +51,8 @@ func logs_info(section: String, message: String, tags: Dictionary = {}) -> void:
 	if 2 >= otel_manager.GDS_GetLogLevelType() :
 		otel_manager.GDS_LogInformation(section,message,tags)
 		print_colored("information",section,message)
-		
 
-func logs_Warn(section: String, message: String, tags: Dictionary = {}) -> void:
+func logs_warn(section: String, message: String, tags: Dictionary = {}) -> void:
 	if 3 >= otel_manager.GDS_GetLogLevelType() :
 		otel_manager.GDS_LogWarning(section,message,tags)
 		print_colored("warning",section,message)
@@ -59,29 +61,29 @@ func logs_error(section: String, message: String, tags: Dictionary = {}) -> void
 	if 4 >= otel_manager.GDS_GetLogLevelType() :
 		otel_manager.GDS_LogError(section,message,tags)
 		print_colored("error",section,message)
-	
+
 func logs_crit(section: String, message: String, tags: Dictionary = {}) -> void:
 	if 5 >= otel_manager.GDS_GetLogLevelType() :
 		otel_manager.GDS_LogCritical(section,message,tags)
 		print_colored("critical",section,message)
 
 # Metric
-func create_metric(metricName: String, type: String) -> void:
-	otel_manager.GDS_CreateMetric(metricName,type)
+func create_metric(metric_name: String, type: String) -> void:
+	otel_manager.GDS_CreateMetric(metric_name,type)
 
-func add_to_metric(metricName: String, value: int,tags: Dictionary = {}) -> void:
-	otel_manager.GDS_AddToMetric(metricName,value,tags)
-	
-func add_to_record(metricName: String, value: float,tags: Dictionary = {}) -> void:
-	otel_manager.GDS_RecordHistogram(metricName,value,tags)
+func add_to_metric(metric_name: String, value: int,tags: Dictionary = {}) -> void:
+	otel_manager.GDS_AddToMetric(metric_name,value,tags)
+
+func add_to_record(metric_name: String, value: float,tags: Dictionary = {}) -> void:
+	otel_manager.GDS_RecordHistogram(metric_name,value,tags)
 
 # Traces
 
-func start_trace(traceName: String,tags: Dictionary = {}) -> String:
-	return otel_manager.GDS_StartActivity(traceName,tags)
+func start_trace(trace_name: String,tags: Dictionary = {}) -> String:
+	return otel_manager.GDS_StartActivity(trace_name,tags)
 
-func stop_trace(traceid: String) -> void:
-	otel_manager.GDS_StopActivity(traceid)
+func stop_trace(trace_id: String) -> void:
+	otel_manager.GDS_StopActivity(trace_id)
 
-func add_tags_to_trace(traceid: String,tags: Dictionary = {}) -> void:
-	otel_manager.GDS_AddTagsToActivity(traceid,tags)
+func add_tags_to_trace(trace_id: String,tags: Dictionary = {}) -> void:
+	otel_manager.GDS_AddTagsToActivity(trace_id,tags)
