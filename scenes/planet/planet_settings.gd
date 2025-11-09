cd3e247515fcd867934aa5a87f3fba6d0ff94152
GuillaumeCa <guicarre1@gmail.com>
2025-11-09 18:12:17 +0100

    fix tarsis scenes

--- scenes/planet/planet_settings.gd
+++ scenes/planet/planet_settings.gd
@@ -7,5 +7,7 @@ extends Resource
 @export var terrain_material: Material
 @export var sea_level: int = 300
 @export var has_ocean: bool = false
+@export var has_clouds: bool = false
+@export var cloud_height: int = 1000
 
 @export var terrain_settings: PlanetTerrainSettings
