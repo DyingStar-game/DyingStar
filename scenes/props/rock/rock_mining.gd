659e3871ad9afdcec26183a54b97350f3c2b4703
David Durieux <david@durieux.family>
2025-11-10 22:56:06 +0100

    Change center of rotation + collision cut

--- scenes/props/rock/rock_mining.gd
+++ scenes/props/rock/rock_mining.gd
@@ -1,9 +1,10 @@
-extends Node3D
+extends RigidBody3D
 
 @export var uuid: String = ""
 
 @onready var cutCube = %CSGBox3D
 @onready var cutCube2 = %CSGBox3D2
+@onready var combiner = %CSGCombiner3D
 
 var spawn_position: Vector3 = Vector3.ZERO
 var spawn_rotation: Vector3 = Vector3.UP
@@ -12,7 +13,32 @@ var server_last_position = Vector3.ZERO
 var server_last_global_rotation = Vector3.ZERO
 
 func _ready() -> void:
-	cutCube.position = Vector3(randf_range(1.8, 3.2), 0, 0)
-	cutCube.rotation_degrees = Vector3(0, 0, randf_range(-35, 35))
-	cutCube2.position = Vector3(randf_range(2.5, 3.85), 0, 0)
-	cutCube2.rotation_degrees = Vector3(0, -41.0, randf_range(-30, 30))
+	cutCube.position = Vector3(randf_range(2.2, 3.0), 0, 0)
+	cutCube.rotation_degrees = Vector3(0, 0, randf_range(-30, 30))
+	#cutCube2.position = Vector3(randf_range(2.6, 3.8), 0, 0)
+	#cutCube2.rotation_degrees = Vector3(0, -41.0, randf_range(-30, 30))
+
+	await get_tree().process_frame
+	var meshes = combiner.get_meshes()
+	var mesh: Mesh = meshes[1]
+	var collision_shape = combiner.bake_collision_shape()
+	
+	# update center of mass
+	var arraymesh = mesh.surface_get_arrays(0)[Mesh.ARRAY_VERTEX]
+	var total = Vector3.ZERO
+	for vertice in arraymesh:
+		total += vertice
+	var meshcenter = total / arraymesh.size()
+	
+	var meshinstance = MeshInstance3D.new()
+	meshinstance.mesh = mesh
+	add_child(meshinstance)
+	
+	var collisioninstance = CollisionShape3D.new()
+	collisioninstance.shape = collision_shape
+	add_child(collisioninstance) 
+	
+	meshinstance.position = -meshcenter
+	collisioninstance.position = -meshcenter
+	
+	combiner.queue_free()
