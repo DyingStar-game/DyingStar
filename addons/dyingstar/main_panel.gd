ce9953eadc937223232f4e34b60236e40c756b5b
TheMoye <46683101+TheMoye@users.noreply.github.com>
2025-12-12 23:04:04 +0100

    Minor changes in the DyingStar add-on (#150)
    * Minor changes in the DyingStar add-on
    
    * Comment removal

--- addons/dyingstar/main_panel.gd
+++ addons/dyingstar/main_panel.gd
@@ -1,15 +1,15 @@
 @tool
 extends Control
 
-@onready var devmoderoot = %devmoderoot
+@onready var devmodelist = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer
 
 func _ready() -> void:
 	_on_reload_command_pressed()
 
 func _on_reload_command_pressed() -> void:
 	# read the devmodefolder
-	var children = devmoderoot.get_children()
-	for child in children:
+	var children = devmodelist.get_children()
+	for child in children.slice(1, children.size()):
 		child.free()
 	var i = 0
 	var dir = DirAccess.open("res://levels/devmode")
@@ -20,14 +20,18 @@ func _on_reload_command_pressed() -> void:
 			if dir.current_is_dir():
 				var btn = Button.new()
 				btn.text = file_name
-				btn.size = Vector2(160, 31)
+				btn.size_flags_horizontal = Control.SIZE_FILL
+				btn.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
 				btn.pressed.connect(_on_button_pressed.bind(file_name))
-				btn.position = Vector2(0, i * 40)
 				i += 1
-				devmoderoot.add_child(btn)
+				devmodelist.add_child(btn)
 			file_name = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
+	
+	self.set_focus_mode(Control.FOCUS_ALL)
+	self.grab_focus()
+	self.set_focus_mode(Control.FOCUS_NONE)
 
 
 func _on_button_pressed(file_name: String):
@@ -53,6 +57,12 @@ func _on_button_pressed(file_name: String):
 
 func _on_horizon_clients_pressed(extra_arg_0: int) -> void:
 	var editor = EditorInterface.get_editor_settings()
+	var actual_instance_count: int = editor.get_project_metadata("debug_options", "run_instance_count")
+	var needed_instance_count: int = 1 + extra_arg_0
+	
+	if actual_instance_count == needed_instance_count:
+		return
+	
 	editor.set_project_metadata("debug_options", "run_instance_count", (1 + extra_arg_0))
 	editor.set_project_metadata("debug_options", "multiple_instances_enabled", true)
 	var instances = [
