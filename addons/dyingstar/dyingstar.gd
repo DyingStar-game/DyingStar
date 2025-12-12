ad2c0addd80c230aa8e672f2b6f0f2d310780411
The_Moye <the_moye@hotmail.fr>
2025-12-12 20:47:42 +0100

    Minor changes in the DyingStar add-on

--- addons/dyingstar/dyingstar.gd
+++ addons/dyingstar/dyingstar.gd
@@ -18,23 +18,22 @@ func _disable_plugin() -> void:
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	main_panel_instance = MainPanel.instantiate()
+	add_control_to_bottom_panel(main_panel_instance, "DyingStar")
 	# Add the main panel to the editor's main viewport.
-	EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
+	# EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
 	# Hide the main panel. Very much required.
-	_make_visible(false)
+	# _make_visible(false)
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	if main_panel_instance:
+	#if main_panel_instance:
+		#main_panel_instance.queue_free()
+	remove_control_from_bottom_panel(main_panel_instance)
+	
+	if is_instance_valid(main_panel_instance):
 		main_panel_instance.queue_free()
-
-func _has_main_screen():
-	return true
-
-func _make_visible(visible):
-	if main_panel_instance:
-		main_panel_instance.visible = visible
+		main_panel_instance = null
 
 
 func _get_plugin_name():
@@ -43,4 +42,5 @@ func _get_plugin_name():
 
 func _get_plugin_icon():
 	# Must return some kind of Texture for the icon.
-	return EditorInterface.get_editor_theme().get_icon("Tools", "EditorIcons")
+	#return EditorInterface.get_editor_theme().get_icon("Tools", "EditorIcons")
+	pass
