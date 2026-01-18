4f2081f7de170101bb221904a1bb480026fd9e74
koothka <koothka@gmail.com>
2026-01-18 20:45:43 +0100

    Suspend wwise  if server

--- audio/managers/audio_manager.gd
+++ audio/managers/audio_manager.gd
@@ -3,5 +3,8 @@ extends Node
 func _ready():
 	#Wwise.set_state("background_music", "None")
 	Wwise.post_event("play_bgm", self)
+	
+	# Suspend wwise if serveur (not ideal solution) the ideal solution would to don't initialised
 	if OS.has_feature("dedicated_server"):
 		print("Server instance", OS)
+		Wwise.suspend(true)
