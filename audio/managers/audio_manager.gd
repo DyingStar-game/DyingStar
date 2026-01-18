a5e75e5f817d74d9f2b896d0f6cfe984b43dfb92
Kilian Liccia <licciak@gmail.com>
2026-01-18 15:57:06 +0100

    Fixed music state switching on both server and client instances
    For now the event play_music is also triggered on server debug instance, so it used to play music there as well. To prevent that, we have a music state that's "None" by default (on Wwise) and then the state is updated at startup of the Login Page scene.

--- audio/managers/audio_manager.gd
+++ audio/managers/audio_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
 func _ready():
+	#Wwise.set_state("background_music", "None")
+	Wwise.post_event("play_bgm", self)
 	if OS.has_feature("dedicated_server"):
 		print("Server instance", OS)
