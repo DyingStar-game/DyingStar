a5a350c7ca7cdcf9a1112d5eb038e00ba4c622cb
WarpZone <nioche.nicolas@gmail.com>
2026-06-09 00:03:58 +0200

    Fix gdlint errors (definition order, trailing whitespace, variable names)

--- scenes/props/furniture/danger_light.gd
+++ scenes/props/furniture/danger_light.gd
@@ -7,7 +7,7 @@ var rotation_speed = 10.0
 func _process(delta: float) -> void:
 	if enabled:
 		$danger_light/light_bulb.rotation.x += delta * rotation_speed
-	
+
 	$danger_light/light_bulb/SpotLight3D.visible = enabled
 	$danger_light/light_bulb/SpotLight3D2.visible = enabled
-	
+
